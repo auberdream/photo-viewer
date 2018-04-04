638c057e1652b5b33bcdd7e9d594fecdd24c2d71
Ellie <elliewem@gmail.com>
2018-04-04 15:16:23 +0100

    Add Slide

---
+++ src/Slide.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Slide extends Component {
+  render() {
+    return (
+      <div className="slide">
+      </div>
+    )
+  }
+}
+
+export default Slide;
