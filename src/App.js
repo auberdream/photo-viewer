67c007a85e23c92ecaaf9be438554ef668fb3e17
Ellie <elliewem@gmail.com>
2018-04-04 15:11:27 +0100

    Add initial App and test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ImageSlideshow from './ImageSlideshow'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ImageSlideshow />
+      </div>
+    )
+  }
+}
+
+export default App;
