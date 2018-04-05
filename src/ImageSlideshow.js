e2a05f938e80393aec5e0c7df28073ae44ad1dd3
Ellie <elliewem@gmail.com>
2018-04-05 13:27:06 +0100

    remove ButtonControlBar from ImageSlideshow

--- src/ImageSlideshow.js
+++ src/ImageSlideshow.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
+import Slide from './Slide'
 
 class ImageSlideshow extends Component {
   render() {
     return (
       <div className="main-container">
-        <ButtonControlBar />
         <Slide />
       </div>
     )
