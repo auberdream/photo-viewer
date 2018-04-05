c4d0c2902996cd2ec539036172f167e04844b5a7
Ellie <elliewem@gmail.com>
2018-04-05 10:55:28 +0100

    Add method to retrieve slide from JSON file

--- src/Slide.js
+++ src/Slide.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import Images from './resources/images'
 
 class Slide extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentSlide: 1
+    }
+  }
+
+  retrieveSlide = () => {
+    const x = this
+    const found = Images.find(function(element) {
+      return element.id === x.state.currentSlide
+    })
+    return found.url
+  }
+
   render() {
     return (
       <div className="slide">
+        <img src={this.retrieveSlide()} />
       </div>
     )
   }
