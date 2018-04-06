4731ccd9ae0fbd2010366856793ed2565f6c2109
Ellie <elliewem@gmail.com>
2018-04-06 23:28:39 +0100

    Add onChange event to SearchBar

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import Images from './resources/images';
 
 class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentSearch: ""
+    };
+  };
+
+  handleChange = (event) => {
+    let searchTerm = event.target.value
+    this.setState({
+      currentSearch: searchTerm
+    });
+  }
+
   render() {
     return (
       <div className="searchbar-container">
-        <input type="textbox" placeholder="What are you looking for?" />
+        <input type="textbox" onChange={ this.handleChange } placeholder="What are you looking for?" />
       </div>
     )
   }
