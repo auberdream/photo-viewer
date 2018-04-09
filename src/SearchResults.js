0e212a141bcadd6656e7022d3c5ce0e9b9023024
Ellie <elliewem@gmail.com>
2018-04-09 09:54:28 +0100

    Refactor Slide and SearchResults to be stateless components

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react';
-import Images from './resources/images';
+import React from 'react';
 import Styles from './resources/styles/search-results.css';
 
-class SearchResults extends Component {
-  render() {
-    return (
-      <div className="searchresults-container">
-        { this.props.currentSearch.map((item, index) =>
-          <div className="search-result" key={ index } onClick={ () => this.props.handler(item.id) }>
-            <img className="search-img" src={ item.url } />
-            <span>{ item.first_name + " " + item.last_name }</span>
-          </div>
-        )}
+const SearchResults = props =>(
+  <div className="searchresults-container">
+    { props.currentSearch.map((item, index) =>
+      <div className="search-result" key={ index } onClick={ () => props.handler(item.id) }>
+        <img className="search-img" src={ item.url } />
+        <span>{ item.first_name + " " + item.last_name }</span>
       </div>
-    )
-  }
-}
+    )}
+  </div>
+);
 
 export default SearchResults;
--- src/Slide.js
+++ src/Slide.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SlideInformation from './SlideInformation'
 import Styles from './resources/styles/slide.css'
 
-class Slide extends Component {
-  render() {
-    return (
-      <div className="slide">
-        <img src={ this.props.slide.url } />
-        <SlideInformation slide={ this.props.slide } />
-      </div>
-    )
-  }
-}
+const Slide = props => (
+  <div className="slide">
+    <img src={ props.slide.url } />
+    <SlideInformation slide={ props.slide } />
+  </div>
+);
+
 
 export default Slide;
